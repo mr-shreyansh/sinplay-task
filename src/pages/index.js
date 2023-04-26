8e200e4166c887791f332142ba37ae03b258ebbe
mr-shreyansh <shreyansh1881@kgpian.iitkgp.ac.in>
2023-04-26 10:44:11 +0530

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,117 +1,157 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
+import images from '../data/images';
+import { useEffect, useState } from 'react';
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [query, setQuery] = useState('');
+
+  // A state variable to store the id of the selected image
+  const [selectedId, setSelectedId] = useState(null);
+  const [pics, setPics] = useState([]);
+  const [newPic, setNewPic] = useState({
+    id: pics.length + 1,
+    url: '',
+    description: ''
+  });
+
+
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        const data = await import('../data/images');
+        setPics(data.images);
+       
+      }
+      catch (error) {
+        console.log(error);
+      }
+
+    };
+    fetchData();
+
+  }, []);
+
+
+  const handleClick = (id) => {
+    // Set the selected id to the clicked image id
+    if (selectedId === null)
+      setSelectedId(id);
+    else {
+      setSelectedId(null)
+    }
+
+  };
+
+  const handleUrlChange = (event) => {
+    // get the new url from the event target
+    const newUrl = event.target.value
+    // update the state with the new url and the old description
+    setNewPic({
+      url: newUrl,
+      description: newPic.description
+    })
+  }
+  
+  const handleDescriptionChange = (event) => {
+    // get the new description from the event target
+    const newDescription = event.target.value
+    // update the state with the new description and the old url
+    setNewPic({
+      url: newPic.url,
+      description: newDescription
+    })
+  }
+  
+  const updatePics = () => {
+    // add the new pic to the pics array
+    console.log(pics)
+    setPics([...pics, newPic])
+    updateData(pics);
+    // reset the newPic state
+    setNewPic({
+      id: pics.length + 1,
+      url: '',
+      description: ''
+    })
+  }
+   
+const handleDragEnd = (result) => {
+  if (!result.destination) return;
+  const items = Array.from(pics);
+  const [reorderedItem] = items.splice(result.source.index, 1);
+  items.splice(result.destination.index, 0, reorderedItem);
+  setPics(items);
+};
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main>
+      <div className='flex justify-center'>
+        <input type='text' className='self-center px-3 py-1 my-5 bg-gray-100 border' placeholder='search' onChange={(e) => setQuery(e.target.value)} />
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+         <DragDropContext onDragEnd={handleDragEnd}>
+          <Droppable droppableId='list'>
+           {
+             (provided) => (
+              <list {...provided.droppableProps} ref={provided.innerRef} className='flex flex-col items-center gap-2 sm:gap-7 sm:flex-row sm:flex-wrap sm:justify-center'>
+              {pics.filter((val) => {
+                if (query == "")
+                  return val
+                else if (val.description.toLowerCase().includes(query.toLocaleLowerCase())) {
+                  return val
+                }
+              }).map((image, index) => (
+                <Draggable key={image.id} draggableId={image.id.toString()} index={index}>
+                  {
+                    (provided) => (
+                      <div className='relative' ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                  <Image
+                    src={image.url}
+                    alt="des"
+                    width={200}
+                    height={300}
+      
+                    onClick={() => handleClick(image.id)}
+                    className={`relative rounded-lg shadow-lg shadow-gray-400 hover:border hover:border-purple-600 transition-all ease-in  ${selectedId === image.id ? ' z-20' : 'relative z-0'}`}
+                    // Use a ternary operator to apply the scale value based on the selected id
+                    style={{ transform: `scale(${selectedId === image.id ? 1.5 : 1})` }}
+                  />{
+                    selectedId === image.id ?
+                      <p className='absolute z-50 text-lg font-light text-center text-white bottom-3 left-2 right-2'>{image.description}</p>
+                      : null
+                  }
+                </div>
+                    )
+                  }
+                
+                </Draggable>
+              ))}
+          </list>
+            )
+           }
+       
+        </Droppable>
+        </DragDropContext>
+      <div className='flex flex-col bg-gray-200 mt-9'>
+         <h1 className='mt-3 font-light text-center'>Upload your own images using url</h1>
+      <div className='flex flex-col items-center bg-gray-200 md:flex-row md:justify-center md:gap-7'>
+        <input type='text' className='px-3 py-1 my-5 bg-gray-100 border' placeholder='image url' value={newPic.url} onChange={handleUrlChange } />
+        <input type='text' className='px-3 py-1 my-5 bg-gray-100 border' placeholder='image description' value={newPic.description} onChange={handleDescriptionChange}/>
+        <button 
+        className='px-3 py-1 my-5 bg-purple-300 border rounded-md'
+        onClick={()=>{ 
+    setPics([...pics, newPic])
+    setNewPic({
+      id: pics.length + 1,
+      url: '',
+      description: ''
+    })}}>submit</button>
+        
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
